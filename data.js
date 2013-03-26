4e7b9b489881739dfdda7c8e31dadda2eb148b4b
Josh Jensen <josh@pixelflavor.com>
2013-03-26 10:22:41 -0400

    All at once.
    I know this is bad form but I am in a hurry.

--- data.js
+++ data.js
@@ -1,19 +1,17 @@
-// See the README (https://github.com/madrobby/microjs.com#readme) for details about how
-// to use this file.
-// Make sure you run 'make compile' to check that your library doesn't come up with
-// warnings or errors. It would also be appreciated if you could check any versioned
-// libraries to see if they have new releases.
-// As per the README, the source should be a link to your unminified, raw, source. It can
-// also be just the raw JS if it's small enough, an array of source files or a ZIP file.
-// See examples below.
-
-var MicroJS = [
-  {
-    name: "Colors.js",
-    github: "matthewbj/Colors",
-    tags: ["color", "color manipulation"],
-    description: "Colors.js is an easy to use color-manipulation library that is lightweight and very functional.",
-    url: "http://matthewbj.github.com/Colors/",
-    source: "https://raw.github.com/matthewbj/Colors/master/colors.js"
-  }
-];
+[
+	{
+		"type": "widget",
+		"tags": ["detail", "navigation"],
+		"url": "https://github.com/mcongrove/ChariTi/tree/1.1.1/app/widgets/com.chariti.detailNavigation",
+		"source": "https://raw.github.com/mcongrove/ChariTi/1.1.1/app/widgets/com.chariti.detailNavigation/widget.json"
+	},
+	{
+		"type": "adapter",
+		"name": "Alloy-Parse-Adapter",
+		"author": "Aaron Saunders",
+		"github": "centogram/Alloy-Parse-Adapter",
+		"tags": ["alloy", "parse"],
+		"description": "Appcelerator Alloy Model Sync Adapter Example for Parse.com API",
+		"url": "https://github.com/centogram/Alloy-Parse-Adapter/tree/master/app"
+	}
+]
